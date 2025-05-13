a0953f58ca2beee3b7e1206dc59a00797b7f9d05
GitSiege7 <carljklug@icloud.com>
2025-05-13 18:43:02 -0400

    added gun

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from constants import *
 from player import *
 from asteroid import *
 from asteroidfield import *
+from shot import *
 
 def main():
     print("Starting Asteroids!")
@@ -20,10 +21,14 @@ def main():
 
     asteroids = pygame.sprite.Group()
 
+    shots = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
 
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = updatable
+
+    Shot.containers = (updatable, drawable)
 
     field = AsteroidField()
 
